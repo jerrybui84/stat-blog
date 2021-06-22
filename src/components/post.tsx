84e0a3f3ccff3ab355360409d4f803d46778036e
jerrybui84 <jerry.bui.stat@gmail.com>
2021-06-22 13:44:17 +0000

    revise slug

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -71,7 +71,7 @@ const Post = ({ data: { post } }: PostProps) => (
       <LazyLoad offsetTop={400}>
         <ReactDisqusComments
           shortname= "jerrybui"
-          identifier= {post.title}
+          identifier= {post.slug}
           title= {post.title}
         />
       </LazyLoad>
